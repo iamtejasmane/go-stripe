2db566e51d3f9ccc321c8136e8ee3974d649410b
Tejas Mane <tejasmane485@gmail.com>
2023-02-03 18:13:26 +0530

    virtual terminal page added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infolog.Println("Hit the Terminal")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorlog.Println(err)
+	}
 }
